380f2ca432ed0e846c7c3cc0f8edfb6d2c2fe1fe
Federico Perini <federico.perini@gmail.com>
2022-10-31 09:06:18 +0100

    Refactor fporde (x,y  sorting)

--- src/fitpack_core.f90
+++ src/fitpack_core.f90
@@ -7810,51 +7810,49 @@ module fitpack_core
       end subroutine fpopsp
 
 
-      recursive subroutine fporde(x,y,m,kx,ky,tx,nx,ty,ny,nummer, &
-         index,nreg)
-      !  subroutine fporde sorts the data points (x(i),y(i)),i=1,2,...,m
-      !  according to the panel tx(l)<=x<tx(l+1),ty(k)<=y<ty(k+1), they belong
-      !  to. for each panel a stack is constructed  containing the numbers
-      !  of data points lying inside; index(j),j=1,2,...,nreg points to the
-      !  first data point in the jth panel while nummer(i),i=1,2,...,m gives
-      !  the number of the next data point in the panel.
-      !  ..
+      !  subroutine fporde sorts the data points (x(i),y(i)),i=1,2,...,m according to the panel
+      !  tx(l)<=x<tx(l+1),ty(k)<=y<ty(k+1), they belong to. for each panel a stack is constructed
+      !  containing the numbers of data points lying inside; index(j),j=1,2,...,nreg points to the first
+      !  data point in the jth panel while nummer(i),i=1,2,...,m gives the number of the next data point
+      !  in the panel.
+      pure subroutine fporde(x,y,m,kx,ky,tx,nx,ty,ny,nummer,index,nreg)
       !  ..scalar arguments..
-      integer m,kx,ky,nx,ny,nreg
+      integer,     intent(in)  :: m,kx,ky,nx,ny,nreg
       !  ..array arguments..
-      real(RKIND) x(m),y(m),tx(nx),ty(ny)
-      integer nummer(m),index(nreg)
+      real(RKIND), intent(in)  :: x(m),y(m),tx(nx),ty(ny)
+      integer,     intent(out) :: nummer(m),index(nreg)
       !  ..local scalars..
-      real(RKIND) xi,yi
-      integer i,im,k,kx1,ky1,k1,l,l1,nk1x,nk1y,num,nyy
+      real(RKIND) :: xi,yi
+      integer :: i,im,k,kx1,ky1,k1,l,l1,nk1x,nk1y,num,nyy
+
       !  ..
       kx1 = kx+1
       ky1 = ky+1
       nk1x = nx-kx1
       nk1y = ny-ky1
       nyy = nk1y-ky
-      do 10 i=1,nreg
-        index(i) = 0
-  10  continue
-      do 60 im=1,m
-        xi = x(im)
-        yi = y(im)
-        l = kx1
-        l1 = l+1
-  20    if(xi<tx(l1) .or. l==nk1x) go to 30
-        l = l1
-        l1 = l+1
-        go to 20
-  30    k = ky1
-        k1 = k+1
-  40    if(yi<ty(k1) .or. k==nk1y) go to 50
-        k = k1
-        k1 = k+1
-        go to 40
-  50    num = (l-kx1)*nyy+k-ky
-        nummer(im) = index(num)
-        index(num) = im
-  60  continue
+      index = 0
+
+      points: do im=1,m
+         xi = x(im)
+         yi = y(im)
+         l = kx1
+         l1 = l+1
+         do while (.not.(xi<tx(l1) .or. l==nk1x))
+            l = l1
+            l1 = l+1
+         end do
+
+         k = ky1
+         k1 = k+1
+         do while (.not.(yi<ty(k1) .or. k==nk1y))
+            k = k1
+            k1 = k+1
+         end do
+         num = (l-kx1)*nyy+k-ky
+         nummer(im) = index(num)
+         index(num) = im
+      end do points
       return
       end subroutine fporde
 
