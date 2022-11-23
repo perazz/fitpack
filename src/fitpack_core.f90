058fbaac59a9c5bbf21d50ec44aab5597115961c
Federico Perini <federico.perini@gmail.com>
2022-11-23 15:06:08 +0100

    fpchep: check periodic spline

--- src/fitpack_core.f90
+++ src/fitpack_core.f90
@@ -618,7 +618,7 @@ module fitpack_core
          t(j2) = t(i2)-per
          t(i1) = t(j1)+per
   60  continue
-      call fpchep(u,m,t,n,k,ier)
+      ier = fpchep(u,m,t,n,k)
       if (ier==FITPACK_OK) go to 80
       go to 90
   70  if(s<0.) go to 90
@@ -2859,89 +2859,93 @@ module fitpack_core
       return
       end subroutine fpched
 
+      ! subroutine fpchep verifies the number and the position of the knots t(j),j=1,2,...,n of a
+      ! periodic spline of degree k, in relation to the number and the position of the data points
+      ! x(i),i=1,2,...,m.
+      ! if all of the following conditions are fulfilled, ier is set to zero.
+      ! if one of the conditions is violated ier is set to ten.
+      pure integer function fpchep(x,m,t,n,k) result(ier)
+          !  ..scalar arguments..
+          integer, intent(in) :: m,n,k
+          !  ..array arguments..
+          real(RKIND), intent(in) :: x(m),t(n)
+          !  ..local scalars..
+          integer :: i,i1,i2,j,j1,k1,k2,l,l1,l2,mm,m1,nk1,nk2
+          real(RKIND) :: per,tj,tl,xi
+          !  ..
+          k1  = k+1
+          k2  = k1+1
+          nk1 = n-k1
+          nk2 = nk1+1
+          m1  = m-1
+          ier = FITPACK_INPUT_ERROR
 
-      recursive subroutine fpchep(x,m,t,n,k,ier)
+          ! 1) k+1 <= n-k-1 <= m+k-1
+          if(nk1<k1 .or. n>m+2*k) return
 
-      !  subroutine fpchep verifies the number and the position of the knots
-      !  t(j),j=1,2,...,n of a periodic spline of degree k, in relation to
-      !  the number and the position of the data points x(i),i=1,2,...,m.
-      !  if all of the following conditions are fulfilled, ier is set
-      !  to zero. if one of the conditions is violated ier is set to ten.
-      !      1) k+1 <= n-k-1 <= m+k-1
-      !      2) t(1) <= t(2) <= ... <= t(k+1)
-      !         t(n-k) <= t(n-k+1) <= ... <= t(n)
-      !      3) t(k+1) < t(k+2) < ... < t(n-k)
-      !      4) t(k+1) <= x(i) <= t(n-k)
-      !      5) the conditions specified by schoenberg and whitney must hold
-      !         for at least one subset of data points, i.e. there must be a
-      !         subset of data points y(j) such that
-      !             t(j) < y(j) < t(j+k+1), j=k+1,...,n-k-1
-      !  ..
-      !  ..scalar arguments..
-      integer m,n,k,ier
-      !  ..array arguments..
-      real(RKIND) x(m),t(n)
-      !  ..local scalars..
-      integer i,i1,i2,j,j1,k1,k2,l,l1,l2,mm,m1,nk1,nk2
-      real(RKIND) per,tj,tl,xi
-      !  ..
-      k1 = k+1
-      k2 = k1+1
-      nk1 = n-k1
-      nk2 = nk1+1
-      m1 = m-1
-      ier = 10
-      !  check condition no 1
-      if(nk1<k1 .or. n>m+2*k) go to 130
-      !  check condition no 2
-      j = n
-      do 20 i=1,k
-        if(t(i)>t(i+1)) go to 130
-        if(t(j)<t(j-1)) go to 130
-        j = j-1
-  20  continue
-      !  check condition no 3
-      do 30 i=k2,nk2
-        if(t(i)<=t(i-1)) go to 130
-  30  continue
-      !  check condition no 4
-      if(x(1)<t(k1) .or. x(m)>t(nk2)) go to 130
-      !  check condition no 5
-      l1 = k1
-      l2 = 1
-      do 50 l=1,m
-         xi = x(l)
-  40     if(xi<t(l1+1) .or. l==nk1) go to 50
-         l1 = l1+1
-         l2 = l2+1
-         if(l2>k1) go to 60
-         go to 40
-  50  continue
-      l = m
-  60  per = t(nk2)-t(k1)
-      do 120 i1=2,l
-         i = i1-1
-         mm = i+m1
-         do 110 j=k1,nk1
-            tj = t(j)
-            j1 = j+k1
-            tl = t(j1)
-  70        i = i+1
-            if(i>mm) go to 120
-            i2 = i-m1
-            if (i2<=0) go to 80
-            go to 90
-  80        xi = x(i)
-            go to 100
-  90        xi = x(i2)+per
- 100        if(xi<=tj) go to 70
-            if(xi>=tl) go to 120
- 110     continue
-         ier = 0
-         go to 130
- 120  continue
- 130  return
-      end subroutine fpchep
+          ! 2) t(1) <= t(2) <= ... <= t(k+1)
+          !    t(n-k) <= t(n-k+1) <= ... <= t(n)
+          j = n
+          do i=1,k
+             if (t(i)>t(i+1)) return
+             if (t(j)<t(j-1)) return
+             j = j-1
+          end do
+
+          ! 3) t(k+1) < t(k+2) < ... < t(n-k)
+          do i=k2,nk2
+             if (t(i)<=t(i-1)) return
+          end do
+
+          ! 4) t(k+1) <= x(i) <= t(n-k)
+          if(x(1)<t(k1) .or. x(m)>t(nk2)) return
+
+          ! 5) the conditions specified by schoenberg and whitney must hold for at least one subset
+          !    of data points, i.e. there must be a subset of data points y(j) such that
+          !        t(j) < y(j) < t(j+k+1), j=k+1,...,n-k-1
+          l1 = k1
+          l2 = 1
+          outer: do l=1,m
+             xi = x(l)
+             do while (.not.(xi<t(l1+1) .or. l==nk1))
+                l1 = l1+1
+                l2 = l2+1
+                if (l2>k1) exit outer
+             end do
+          end do outer
+          if (l2<=k1) l = m
+
+          per = t(nk2)-t(k1)
+          subset_start: do i1=2,l
+             i = i1-1
+             mm = i+m1
+             subset_inner: do j=k1,nk1
+                tj = t(j)
+                j1 = j+k1
+                tl = t(j1)
+                xi = -huge(xi)
+                do while (xi<=tj)
+                    i = i+1
+                    if (i>mm) cycle subset_start
+                    i2 = i-m1
+
+                    if (i2<=0) then
+                      xi = x(i)
+                    else
+                      xi = x(i2)+per
+                    endif
+                end do
+                if(xi>=tl) cycle subset_start
+             end do subset_inner
+
+             ! A full Shoenberg-Whitney subset is found
+             ier = FITPACK_OK
+             return
+          end do subset_start
+
+          ! No subsets found: return with error
+
+      end function fpchep
 
 
       recursive subroutine fpclos(iopt,idim,m,u,mx,x,w,k,s,nest,tol, &
@@ -3062,9 +3066,7 @@ module fitpack_core
       !  fp0 denotes the corresponding sum of squared residuals.
   35  fp0 = zero
       d1 = zero
-      do 37 j=1,idim
-        z(j) = zero
-  37  continue
+      z(1:idim) = zero
       jj = 0
       do 45 it=1,m1
         wi = w(it)
@@ -3076,9 +3078,7 @@ module fitpack_core
           fp0 = fp0+fac**2
   40    continue
   45  continue
-      do 47 j=1,idim
-        z(j) = z(j)/d1
-  47  continue
+      z(1:idim) = z(1:idim)/d1
       !  test whether that fixed point is a solution of our problem.
       fpms = fp0-s
       if(fpms<acc .or. nmax==nmin) go to 640
@@ -3279,9 +3279,7 @@ module fitpack_core
  140      continue
       !  add contribution of this row to the sum of squares of residual
       !  right hand sides.
- 150      do 155 j2=1,idim
-            fp = fp+xi(j2)**2
- 155      continue
+ 150      fp = fp + sum(xi(1:idim)**2)
  290    continue
         fpint(n) = fp0
         fpint(n-1) = fpold
@@ -14674,7 +14672,7 @@ module fitpack_core
         tu(l2) = tu(l4)-peru
         tu(l3) = tu(l1)+peru
   50  continue
-      call fpchep(u,mu,tu,nu,3,ier)
+      ier = fpchep(u,mu,tu,nu,3)
       if(ier/=0) go to 200
   60  if(nv<8 .or. nv>nvest) go to 200
       vb = v(1)
@@ -14704,7 +14702,7 @@ module fitpack_core
         tv(l2) = tv(l4)-perv
         tv(l3) = tv(l1)+perv
   90  continue
-      call fpchep(v,mv,tv,nv,3,ier)
+      ier = fpchep(v,mv,tv,nv,3)
       if (ier==0) go to 150
       go to 200
  100  if(s<0.) go to 200
@@ -14984,7 +14982,7 @@ module fitpack_core
          t(j2) = t(i2)-per
          t(i1) = t(j1)+per
   20  continue
-      call fpchep(x,m,t,n,k,ier)
+      ier = fpchep(x,m,t,n,k)
       if (ier==0) go to 40
       go to 50
   30  if(s<0.) go to 50
@@ -15439,7 +15437,7 @@ module fitpack_core
         l = l+1
  135  continue
       wrk(l) = ve
-      call fpchep(wrk(9),mv+1,tv,nv,3,ier)
+      ier = fpchep(wrk(9),mv+1,tv,nv,3)
       if (ier==0) go to 150
       go to 200
  140  if(s<zero) go to 200
@@ -16936,7 +16934,7 @@ module fitpack_core
         l = l+1
  135  continue
       wrk(l) = ve
-      call fpchep(wrk(13),mv+1,tv,nv,3,ier)
+      ier = fpchep(wrk(13),mv+1,tv,nv,3)
       if (ier==0) go to 150
       go to 200
  140  if(s<0.) go to 200
